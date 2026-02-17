bf7498da556637152677a0b4db3d01432c3ba464
Atharv <18atharv@gmail.com>
2026-02-18 01:30:56 +0530

    project 001

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
 
 }
